c139973b4a1602044886bd1937b17bfad4fb3569
Walter Adamba Omedo <omedo.walter@eclectics.io>
2026-01-06 12:01:26 +0300

    Authentication Implementation for Client and Server

---
+++ BaseLibrary/DTOs/CustomUserClaims.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BaseLibrary.DTOs
+{
+    public class CustomUserClaims
+    {
+        public record CustomUserClaims(string Id=null!, string Name=null!, string Email = null!, string Role = null! );
+
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -5,40 +5,43 @@ using ServerLibrary.Repositories.Contracts;
 using ServerLibrary.Repositories.Implementations;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
 //Map DB
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Sorry, your connection string not found!"));
 });
-
 //User Repository
 builder.Services.AddScoped<IUserAccount, UserAccountRepository>();
+//Add or Enable CORS
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorWasm", policy =>
+        policy
+            .WithOrigins(allowedOrigins ?? Array.Empty<string>())
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+    );
+});
 
 //Map JWT
 builder.Services.Configure<JwtSection>(builder.Configuration.GetSection("JwtSection"));
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
+app.UseCors("AllowBlazorWasm");
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
