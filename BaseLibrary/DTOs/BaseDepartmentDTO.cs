2661495e6dc2612a3e5ee7e4ccb438935bb7b311
Walter Adamba Omedo <omedo.walter@eclectics.io>
2026-01-23 09:25:39 +0300

    Added Top and Sidebar Menu Items and Department Feature With Dialog

---
+++ BaseLibrary/DTOs/BaseDepartmentDTO.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace BaseLibrary.DTOs
+{
+    public class BaseDepartmentDTO
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(20), MinLength(3)]
+        public string? Name { get; set; }
+    }
+}
